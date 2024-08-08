3bb4dfa3c236c495e9e7d1f0bb17b66298149406
Gasarayev <gasarayev.emil@gmail.com>
2024-08-08 17:32:14 +0400

    photos

---
+++ src/components/client/Photos/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Photos() {
+    // const {data, } =
+  return (
+    <div className='container'>
+        <div className='row'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Photos
