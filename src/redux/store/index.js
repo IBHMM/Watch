170faa3553ddb45bfa773990804ce861f47e1e3e
Ibrahim Muradov <136456526+IBHMM@users.noreply.github.com>
2024-08-08 16:52:22 +0400

    reducers

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { PopularItems } from '../api/apipopularitems';
 import { NewArrivals } from '../api/apinewarrivals';
+import { Choises } from '../api/apichoise';
+import { Photos } from '../api/apiphotos';
 
 
 const store = configureStore({
   reducer: {
     [PopularItems.reducerPath]: PopularItems.reducer,
     [NewArrivals.reducerPath] : NewArrivals.reducer,
+    [Choises.reducerPath] : Choises.reducer,
+    [Photos.reducerPath] : Photos.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(PopularItems.middleware, NewArrivals.middleware),
